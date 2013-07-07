5cd4f19917e881286072dde0f50b1d557eb0512a
Elias Vazquez <rashgaroc@gmail.com>
2013-07-07 18:30:43 +0200

    Version 1

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Rsh-startup-eng! v2');
+  response.send('Hello World! Rsh-startup-eng! v1');
 });
 
 var port = process.env.PORT || 5000;
